db714ba6f7e35131d0026937d568ce116e97e26c
Nikita Tsyhanenko <runninzero@gmail.com>
2016-10-08 14:04:07 +0300

    test of sass compiler

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const Promise = require('bluebird');
+const sass = Promise.promisifyAll(require('node-sass'));
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/test', (req, res) => {
+    sass.renderAsync({
+        data: '@import "bourbon"; body { @include transform(translateX(50%)); }',
+        includePaths: require("bourbon").includePaths
+    }).then((result) => {
+        res.end(result.css);
+    });
+});
 
 app.listen(3000, function () {
     console.log('Running on localhost:3000');
