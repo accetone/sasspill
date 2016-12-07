f971586a6b6a3e56e44ab3857cef18f09f05bc04
Nikita Tsyhanenko <runninzero@gmail.com>
2016-12-07 17:21:04 +0300

    API: enable gzip #8

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const compress = require('compression');
 
 const app = express();
 const api = require('./api');
 
 const port = process.env.PORT || 3000;
 
+app.use(compress());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use('/api', api);
